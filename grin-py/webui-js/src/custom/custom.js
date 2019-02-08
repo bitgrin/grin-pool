9f751525ccdb757fc90059ae58c5a66252c4c3c3
motelddir <47373416+motelddir@users.noreply.github.com>
2019-02-08 05:46:17 +0100

    Add files via upload

--- grin-py/webui-js/src/custom/custom.js
+++ grin-py/webui-js/src/custom/custom.js
@@ -1,4 +1,4 @@
 export const colorTheme = 'theme-dark'
 export const C29_COLOR = '#fba330'
 export const C31_COLOR = '#70d0fd'
-export const POOL_NAME = 'Mining Pool'
+export const POOL_NAME = 'BitGrin Mining Pool'
